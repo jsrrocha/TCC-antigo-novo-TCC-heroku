78b28bf7b8c03b206cc1d24b1b1839dbc7b8388a
jsrrocha <32655487+jsrrocha@users.noreply.github.com>
2019-04-26 04:20:55 -0300

    fixes cross

--- CadeMeuBichinho/src/main/java/com/tcc/CadeMeuBichinho/CadeMeuBichinhoApplication.java
+++ CadeMeuBichinho/src/main/java/com/tcc/CadeMeuBichinho/CadeMeuBichinhoApplication.java
@@ -2,6 +2,12 @@ package com.tcc.CadeMeuBichinho;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class CadeMeuBichinhoApplication {
@@ -9,6 +15,20 @@ public class CadeMeuBichinhoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CadeMeuBichinhoApplication.class, args);
 	}
+	
+	@Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean; 
+   }
 
 }
 
